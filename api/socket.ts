99235d71a6ba55f2c55ef5ea9e82c5db7aadfa99
petya0927 <unyipeti@gmail.com>
2023-11-22 18:33:40 +0100

    Refact

--- api/socket.ts
+++ api/socket.ts
@@ -1,3 +1,4 @@
+import { Room, User } from '@/app/types';
 import { Socket, io } from 'socket.io-client';
 
 let socket: Socket;
@@ -17,4 +18,62 @@ export const initSocket = ({
   });
 
   return socket;
+};
+
+const isUserInRoom = ({ user, room }: { user: User; room?: Room }) => {
+  return room?.users.find((u: User) => u.username === user.username)
+    ? true
+    : false;
+};
+
+export const onRoomData = ({
+  setRoom,
+  data,
+}: {
+  setRoom: React.Dispatch<React.SetStateAction<Room | undefined>>;
+  data: Room;
+}) => {
+  setRoom(data);
+};
+
+export const onUserJoined = ({
+  setRoom,
+  data,
+}: {
+  setRoom: React.Dispatch<React.SetStateAction<Room | undefined>>;
+  data: User;
+}) => {
+  setRoom((prev) => {
+    if (prev) {
+      if (!isUserInRoom({ user: data, room: prev })) {
+        return {
+          ...prev,
+          users: [...prev.users, data],
+        };
+      }
+    }
+
+    return prev;
+  });
+};
+
+export const onUserLeft = ({
+  setRoom,
+  data,
+}: {
+  setRoom: React.Dispatch<React.SetStateAction<Room | undefined>>;
+  data: User;
+}) => {
+  setRoom((prev) => {
+    if (prev) {
+      if (isUserInRoom({ user: data, room: prev })) {
+        return {
+          ...prev,
+          users: prev.users.filter((u) => u.username !== data.username),
+        };
+      }
+    }
+
+    return prev;
+  });
 };
--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import ReactPlayer from 'react-player';
-import { initSocket } from '@/api/socket';
+import { initSocket, onRoomData, onUserJoined, onUserLeft } from '@/api/socket';
 import { useEffect, useState } from 'react';
 import { Room, User } from '@/app/types';
 import { useSearchParams } from 'next/navigation';
@@ -11,63 +11,18 @@ export default function Room({
   params: { id: string; username: string };
 }) {
   const searchParams = useSearchParams();
-
-  const [room, setRoom] = useState<Room>();
   const username = searchParams.get('username');
 
+  const [room, setRoom] = useState<Room>();
+
   useEffect(() => {
-    const isUserInRoom = ({ user, room }: { user: User; room?: Room }) => {
-      console.log('isUserInRoom', user, room);
-      return room?.users.find((u: User) => u.username === user.username)
-        ? true
-        : false;
-    };
-
-    const onRoomData = (data: Room) => {
-      setRoom(data);
-    };
-
-    const onUserJoined = (data: User) => {
-      setRoom((prev) => {
-        if (prev) {
-          if (!isUserInRoom({ user: data, room: prev })) {
-            return {
-              ...prev,
-              users: [...prev.users, data],
-            };
-          }
-        }
-
-        return prev;
-      });
-    };
-
-    const onUserLeft = (data: User) => {
-      setRoom((prev) => {
-        if (prev) {
-          if (isUserInRoom({ user: data, room: prev })) {
-            return {
-              ...prev,
-              users: prev.users.filter((u) => u.username !== data.username),
-            };
-          }
-        }
-
-        return prev;
-      });
-    };
-
-    if (!params.id || !username) {
-      return;
-    }
-
     const socket = initSocket({ id: params.id, username: username as string });
 
-    socket.on('room-data', onRoomData);
+    socket.on('room-data', (data: Room) => onRoomData({ setRoom, data }));
 
-    socket.on('user-joined', onUserJoined);
+    socket.on('user-joined', (data: User) => onUserJoined({ setRoom, data }));
 
-    socket.on('user-left', onUserLeft);
+    socket.on('user-left', (data: User) => onUserLeft({ setRoom, data }));
 
     return () => {
       socket.disconnect();
@@ -86,7 +41,7 @@ export default function Room({
             .map((user) => `${user.username}: ${user.socketId}`)
             .join(', ')}
         </p>
-        {/* <ReactPlayer url={data.room.videoUrl} /> */}
+        <ReactPlayer url={room.videoUrl} controls pip />
       </div>
     )
   );
