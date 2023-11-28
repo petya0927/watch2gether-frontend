77128d62715158f3c8981221479e169705dbd789
petya0927 <unyipeti@gmail.com>
2023-11-28 23:47:08 +0100

    Update tailwind.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,15 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
-    screens: {
-      sm: "426px",
+    extend: {
+      screens: {
+        sm: '426px',
+      },
     },
   },
   plugins: [],
