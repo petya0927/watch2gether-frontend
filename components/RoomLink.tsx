9951ec2646b912b1d7e6c0d38c90d50d40e28222
petya0927 <unyipeti@gmail.com>
2023-11-29 23:30:50 +0100

    Refactor room link URL to remove protocol

--- components/RoomLink.tsx
+++ components/RoomLink.tsx
@@ -4,8 +4,10 @@ import { IconCheck, IconCopy } from '@tabler/icons-react';
 const RoomLink = ({ roomLink }: { roomLink: string }) => {
   const url = new URL(roomLink);
   url.searchParams.delete('username');
-
-  const cleanRoomLink = url.toString();
+  const cleanRoomLink = url
+    .toString()
+    .replace('http://', '')
+    .replace('https://', '');
 
   return (
     <div className="bg-white flex flex-col gap-4 items-start rounded-2xl p-4 w-full shadow-lg">
