eb85884e216cd5ce4870d39e36fe4000400a95df
petya0927 <unyipeti@gmail.com>
2023-11-29 22:44:16 +0100

    Refactor video player component and socket event listeners

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,21 +1,16 @@
 'use client';
 import {
-  handlePlaybackRateChange,
-  handlePlayerPause,
-  handlePlayerPlay,
   initSocket,
   onRoomDataEvent,
   onUserJoinedEvent,
   onUserLeftEvent,
-  onVideoPause,
-  onVideoPlay,
-  onVideoPlaybackRateChange,
 } from '@/api/socket';
-import { Player, Room, User } from '@/app/types';
+import { Room, User } from '@/app/types';
 import Users from '@/components/Users';
+import VideoPlayer from '@/components/VideoPlayer';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
-import ReactPlayer from 'react-player';
+import { useEffect, useState } from 'react';
+import { Socket } from 'socket.io-client';
 
 export default function Room({
   params,
@@ -25,79 +20,41 @@ export default function Room({
   const searchParams = useSearchParams();
   const username = searchParams.get('username');
 
-  const reactPlayerRef = useRef<ReactPlayer | null>(null);
-
   const [room, setRoom] = useState<Room>();
-  const [player, setPlayer] = useState<Player>({
-    isPlaying: false,
-    isPlayingFromSocket: false,
-    playbackRate: 1,
-    reactPlayerRef,
-  });
+  const [socket, setSocket] = useState<Socket>();
 
   useEffect(() => {
-    const socket = initSocket({ id: params.id, username: username as string });
+    const socketInstance = initSocket({
+      id: params.id,
+      username: username as string,
+    });
 
-    socket.on('room-data', (data: Room) => onRoomDataEvent({ setRoom, data }));
+    setSocket(socketInstance);
 
-    socket.on('user-joined', (data: User) =>
+    socketInstance.on('room-data', (data: Room) =>
+      onRoomDataEvent({ setRoom, data })
+    );
+
+    socketInstance.on('user-joined', (data: User) =>
       onUserJoinedEvent({ setRoom, data })
     );
 
-    socket.on('user-left', (data: User) => onUserLeftEvent({ setRoom, data }));
-
-    socket.on('video-play', (data) =>
-      onVideoPlay({ setPlayer, reactPlayerRef, data })
-    );
-
-    socket.on('video-pause', () => onVideoPause({ setPlayer }));
-
-    socket.on('video-playback-rate-change', (data) =>
-      onVideoPlaybackRateChange({ setPlayer, data })
+    socketInstance.on('user-left', (data: User) =>
+      onUserLeftEvent({ setRoom, data })
     );
 
     return () => {
-      socket.off('room-data');
-      socket.off('user-joined');
-      socket.off('user-left');
-      socket.off('video-play');
-      socket.off('video-pause');
-      socket.off('video-progress');
-      socket.disconnect();
+      socketInstance.off('room-data');
+      socketInstance.off('user-joined');
+      socketInstance.off('user-left');
+      socketInstance.disconnect();
     };
   }, [params.id, username]);
 
   return (
     room && (
       <div className="flex flex-col lg:flex-row gap-8 items-center justify-start lg:justify-center h-full w-full">
-        <div className="w-screen sm:w-full md:w-2/3 md:max-w-2xl aspect-video -m-4 mb-0 sm:m-0 sm:rounded-2xl sm:overflow-hidden">
-          <ReactPlayer
-            className="h-full w-full"
-            ref={reactPlayerRef}
-            url={room.videoUrl}
-            controls
-            playing={player.isPlaying}
-            playbackRate={player.playbackRate}
-            onPlay={() =>
-              handlePlayerPlay({
-                player,
-                setPlayer,
-              })
-            }
-            onPause={() =>
-              handlePlayerPause({
-                player,
-                setPlayer,
-              })
-            }
-            onPlaybackRateChange={(playbackRate: number) =>
-              handlePlaybackRateChange({
-                playbackRate,
-                setPlayer,
-              })
-            }
-          />
-        </div>
+        <VideoPlayer room={room} socket={socket} />
         <Users room={room} />
       </div>
     )
---
+++ components/VideoPlayer.tsx
@@ -0,0 +1,79 @@
+import {
+  handlePlaybackRateChange,
+  handlePlayerPause,
+  handlePlayerPlay,
+  onVideoPause,
+  onVideoPlay,
+  onVideoPlaybackRateChange,
+} from '@/api/socket';
+import { Player, Room } from '@/app/types';
+import { useEffect, useRef, useState } from 'react';
+import ReactPlayer from 'react-player';
+import { Socket } from 'socket.io-client';
+
+interface VideoPlayerComponentProps {
+  room: Room;
+  socket: Socket | undefined;
+}
+
+const VideoPlayer = ({ room, socket }: VideoPlayerComponentProps) => {
+  const reactPlayerRef = useRef<ReactPlayer | null>(null);
+
+  const [player, setPlayer] = useState<Player>({
+    isPlaying: false,
+    isPlayingFromSocket: false,
+    playbackRate: 1,
+    reactPlayerRef,
+  });
+
+  useEffect(() => {
+    socket?.on('video-play', (data) =>
+      onVideoPlay({ setPlayer, reactPlayerRef, data })
+    );
+
+    socket?.on('video-pause', () => onVideoPause({ setPlayer }));
+
+    socket?.on('video-playback-rate-change', (data) =>
+      onVideoPlaybackRateChange({ setPlayer, data })
+    );
+
+    return () => {
+      socket?.off('video-play');
+      socket?.off('video-pause');
+      socket?.off('video-playback-rate-change');
+    };
+  }, [socket]);
+
+  return (
+    <div className="w-screen sm:w-full md:w-2/3 md:max-w-2xl aspect-video -m-4 mb-0 sm:m-0 sm:rounded-2xl sm:overflow-hidden">
+      <ReactPlayer
+        className="h-full w-full"
+        ref={reactPlayerRef}
+        url={room.videoUrl}
+        controls
+        playing={player.isPlaying}
+        playbackRate={player.playbackRate}
+        onPlay={() =>
+          handlePlayerPlay({
+            player,
+            setPlayer,
+          })
+        }
+        onPause={() =>
+          handlePlayerPause({
+            player,
+            setPlayer,
+          })
+        }
+        onPlaybackRateChange={(playbackRate: number) =>
+          handlePlaybackRateChange({
+            playbackRate,
+            setPlayer,
+          })
+        }
+      />
+    </div>
+  );
+};
+
+export default VideoPlayer;
