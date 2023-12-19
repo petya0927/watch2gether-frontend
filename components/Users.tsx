85ff0440b0e5e01f3420379479fbe4922eadcd1a
petya0927 <unyipeti@gmail.com>
2023-12-19 20:24:39 +0100

    Remove filtering of users without owner in Users component

--- components/Users.tsx
+++ components/Users.tsx
@@ -6,16 +6,11 @@ interface UsersComponentProps {
 }
 
 const Users = ({ room }: UsersComponentProps) => {
-  const usersWithoutOwner = room.users.filter(
-    (user) => user.username !== room.owner
-  );
-
   return (
     <div className="bg-white flex flex-col gap-2 items-start rounded-2xl p-4 w-full shadow-lg">
       <h2 className="font-bold text-xl">Users</h2>
-      <User username={room.owner} isOwner />
 
-      {usersWithoutOwner.map((user) => (
+      {room.users.map((user) => (
         <User
           key={user.socketId}
           username={user.username}
