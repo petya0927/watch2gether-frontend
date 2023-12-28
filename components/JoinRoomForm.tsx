0a2ffb14bf1fc4ad87920c8591224e948444c21b
petya0927 <unyipeti@gmail.com>
2023-12-28 23:37:12 +0100

    Fix room existence check error and add URL validation function

--- components/JoinRoomForm.tsx
+++ components/JoinRoomForm.tsx
@@ -1,4 +1,5 @@
 import { handleIsUsernameTaken, isRoomExists } from '@/api/rooms';
+import { isValidUrl } from '@/utils/helperFunctions';
 import { Button, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { IconArrowRight, IconLink } from '@tabler/icons-react';
@@ -26,26 +27,45 @@ const JoinRoomForm = ({ username, setUsernameError }: JoinRoomFormProps) => {
         }
       },
       url: (value) => {
-        let url = new URL(value);
-        const roomId = url.pathname.split('/')[2];
+        if (isValidUrl(value)) {
+          let url = new URL(value);
+          const roomId = url.pathname.split('/')[2];
 
-        if (
-          roomId.length !== 24 ||
-          !value.includes(window.location.host + '/room/' + roomId)
-        ) {
-          return 'Please enter a valid room link';
-        } else if (roomExistsError) {
-          return roomExistsError;
-        }
+          if (!roomId) {
+            return 'Please enter a valid room link';
+          } else if (
+            roomId.length !== 24 ||
+            !value.includes(window.location.host + '/room/' + roomId)
+          ) {
+            return 'Please enter a valid room link';
+          } else if (roomExistsError) {
+            return roomExistsError;
+          }
+        } else return 'Please enter a valid room link';
       },
+    },
+    onValuesChange: (values) => {
+      if (values.url) {
+        if (isValidUrl(values.url)) {
+          let url = new URL(values.url, window.location.href);
+          url.searchParams.delete('username');
+          values.url = url.toString();
+        }
+        return values;
+      }
+    },
+    transformValues: (values) => {
+      if (!values.url.startsWith('http') && !values.url.startsWith('https')) {
+        values.url = window.location.protocol + '//' + values.url;
+      }
+
+      return values;
     },
   });
 
   useEffect(() => {
-    const roomId = existingRoomForm.values.url
-      .trim()
-      .split('/')
-      .pop() as string;
+    const roomId = existingRoomForm.values.url.trim().split('/').pop();
+    setUsernameError('');
 
     if (roomId && roomId.length === 24) {
       const checkUsernameAvailability = async () => {
@@ -69,15 +89,12 @@ const JoinRoomForm = ({ username, setUsernameError }: JoinRoomFormProps) => {
       };
 
       const validateRoomExistence = async () => {
-        const roomId = existingRoomForm.values.url
-          .trim()
-          .split('/')
-          .pop() as string;
-
         if (roomId) {
           const isExists = await isRoomExists(roomId);
           if (!isExists) {
             setRoomExistsError('Room does not exist');
+          } else if (isExists === undefined) {
+            setRoomExistsError('Something went wrong. Please try again later');
           } else {
             setRoomExistsError('');
           }
@@ -92,11 +109,7 @@ const JoinRoomForm = ({ username, setUsernameError }: JoinRoomFormProps) => {
   }, [username, existingRoomForm.values.url]);
 
   const handleJoinRoom = () => {
-    let url = existingRoomForm.values.url;
-    if (!url.startsWith('http://') && !url.startsWith('https://')) {
-      url = window.location.protocol + '//' + url;
-    }
-    router.push(`${url}?username=${username}`);
+    router.push(`${existingRoomForm.values.url}?username=${username}`);
   };
 
   return (
@@ -113,12 +126,13 @@ const JoinRoomForm = ({ username, setUsernameError }: JoinRoomFormProps) => {
           leftSection={<IconLink size={20} />}
           classNames={{
             root: 'w-2/3 sm:w-1/2',
-            input: `!bg-transparent border-1 rounded-md ${
-              existingRoomForm.errors.existingRoomUrl
-                ? 'border-red-500 text-red-500'
+            input: `!bg-transparent rounded-md ${
+              existingRoomForm.errors.url
+                ? 'border-red-500 text-red-500 border-1'
                 : 'border-white text-white'
             }`,
           }}
+          error={existingRoomForm.errors.url}
           {...existingRoomForm.getInputProps('url')}
         />
         <Button
