f87660a30b705c4a1685818ac0d33681bf2e3557
petya0927 <unyipeti@gmail.com>
2023-12-31 08:06:58 +0100

    Add auto scrolling to bottom of chat when new message

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,6 +1,7 @@
 import { Message } from '@/app/types';
 import ChatInput from './ChatInput';
 import ChatMessage from './ChatMessage';
+import { useEffect, useRef } from 'react';
 
 interface ChatProps {
   messages: Message[];
@@ -9,6 +10,12 @@ interface ChatProps {
 }
 
 const Chat = ({ messages, addMessage, username }: ChatProps) => {
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
   return (
     <div className="bg-white flex flex-col gap-2 items-start rounded-2xl p-4 w-full md:w-1/3 shadow-lg h-full max-h-96 md:max-h-[79vh]">
       <h2 className="font-bold text-xl">Chat</h2>
@@ -16,6 +23,7 @@ const Chat = ({ messages, addMessage, username }: ChatProps) => {
         {messages.map((message) => (
           <ChatMessage key={message.id} message={message} />
         ))}
+        <div ref={bottomRef} />
       </div>
       <ChatInput addMessage={addMessage} username={username} />
     </div>
