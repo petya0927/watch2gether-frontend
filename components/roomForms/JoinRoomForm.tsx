69642343b775f0835fc6a126f406a80b7f9b433e
petya0927 <unyipeti@gmail.com>
2024-02-23 10:00:48 +0100

    Trying to solve disabled styles

--- components/roomForms/JoinRoomForm.tsx
+++ components/roomForms/JoinRoomForm.tsx
@@ -146,7 +146,7 @@ const JoinRoomForm = ({ username, setUsernameError }: JoinRoomFormProps) => {
             !existingRoomForm.values.username
           }
           classNames={{
-            root: 'w-auto rounded-md sm:self-start disabled:opacity-50 bg-white disabled:bg-opacity-70',
+            root: 'w-auto rounded-md sm:self-start disabled:opacity-50',
             label: 'flex gap-1 items-center justify-center',
           }}
         >
